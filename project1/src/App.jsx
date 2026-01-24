9af7f7ee04131e38aa5c06cdf789f047b885c417
Raihan Ahmed <raiihan2500@gmail.com>
2026-01-24 15:44:19 +0600

    Class 13: Generating unique id with uuid package using them while creating a Card such that all card can uniquely identify and we can access them easily with this id.

--- project1/src/App.jsx
+++ project1/src/App.jsx
@@ -5,13 +5,17 @@ import './App.css'
 import React from 'react'
 import Card from './components/Card'
 import Data from './data.json'
+import {v4 as uuidv4} from "uuid";
 
 function App(){
+  
   return <div>
     <h1 className='headingStyle'>Todo App</h1>
-    {Data.map((item) => <Card titleText = {item.title} descText = {item.descText} />)}
+    {Data.map((item) => <Card key = {uuidv4()} titleText = {item.title} descText = {item.descText} />)}
 
   </div>
+
+
 }
 
 export default App
