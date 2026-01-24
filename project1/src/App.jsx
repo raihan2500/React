8c67f85afd4dc12f3335982de7f1fc5fd499a566
Raihan Ahmed <raiihan2500@gmail.com>
2026-01-24 15:27:00 +0600

    Class 12: Creating data.json Rendering data from a json file and creted Card with the help of this data

--- project1/src/App.jsx
+++ project1/src/App.jsx
@@ -4,14 +4,15 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import React from 'react'
 import Card from './components/Card'
-
+import Data from './data.json'
 
 function App(){
   return <div>
     <h1 className='headingStyle'>Todo App</h1>
-    <Card titleText="Call mother" descText ="This is description" />
-    <Card titleText="Call father" descText ="This is desc2" />
-    <Card titleText = "Call wife" descText="this is desc3" />
+    <Card titleText= {Data[0].title} descText = {Data[0].desc} />
+    <Card titleText= {Data[1].title} descText = {Data[1].desc} />
+    <Card titleText= {Data[2].title} descText = {Data[2].desc} />
+
 
   </div>
 }
--- project1/src/components/Card.jsx
+++ project1/src/components/Card.jsx
@@ -1,11 +1,5 @@
 const date = new Date();
 
-//Desctructuring Example. 
-const arr = ["Raihan", "Elon Mask"];
-const [raihan, elon] = arr
-console.log(raihan)
-
-
 function Card(props){
   const {titleText, descText} = props;
 
