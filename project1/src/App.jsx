6b05244506a1e4b354d6ca57c5f3fad1dd208f37
Raihan Ahmed <raiihan2500@gmail.com>
2026-01-24 15:30:18 +0600

    Class 12: Performed the same task using for loop.

--- project1/src/App.jsx
+++ project1/src/App.jsx
@@ -7,12 +7,14 @@ import Card from './components/Card'
 import Data from './data.json'
 
 function App(){
+  let items = [];
+  for(let i = 0; i < Data.length; i++){
+    items.push(<Card titleText = {Data[i].title} descText = {Data[i].desc} /> );
+  }
+
   return <div>
     <h1 className='headingStyle'>Todo App</h1>
-    <Card titleText= {Data[0].title} descText = {Data[0].desc} />
-    <Card titleText= {Data[1].title} descText = {Data[1].desc} />
-    <Card titleText= {Data[2].title} descText = {Data[2].desc} />
-
+    {items}
 
   </div>
 }
