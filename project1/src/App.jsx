22c85a48c8a4010662f452d80c6bae5134270bb9
Raihan Ahmed <raiihan2500@gmail.com>
2026-01-24 16:08:45 +0600

    Class 14: Nested list

--- project1/src/App.jsx
+++ project1/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { use, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,13 +7,51 @@ import Card from './components/Card'
 import Data from './data.json'
 import {v4 as uuidv4} from "uuid";
 
+const users = [
+  {
+    fullName: "Raihan Ahmed",
+    age: 22,
+    phones:[
+      {home: "01752205317"},
+      {office: "123"}
+    ]
+  },
+
+  {
+    fullName: "Elon Musk",
+    age: 54,
+    phones:[
+      {home: "1"},
+      {office:"2"}
+    ]
+  }
+]
+
+
 function App(){
   
-  return <div>
-    <h1 className='headingStyle'>Todo App</h1>
-    {Data.map((item) => <Card key = {uuidv4()} titleText = {item.title} descText = {item.descText} />)}
+  return(
+    <div>
+      <h1 style={{margin:"20px", color: "red"}} >Nested Lists</h1>
+      {
+        users.map((user, index)=> 
+        <article key={index}>
+          <h3>{user.fullName}</h3>
+          <p>{user.age} </p>
+          {
+            user.phones.map((phone, index) => 
+            <div>
+              <p>{phone.home} </p>
+              <p>{phone.office} </p>
+            </div>
+            )
+          }
+          <br />
+        </article>)
+      }
 
-  </div>
+    </div>
+  )
 
 
 }
