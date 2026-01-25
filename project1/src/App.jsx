427f49013121b572521886fbfd7d39f6e135bd01
Raihan Ahmed <raiihan2500@gmail.com>
2026-01-25 19:40:25 +0600

    Class 19: How react component works under the hood

--- project1/src/App.jsx
+++ project1/src/App.jsx
@@ -11,14 +11,37 @@ import Card1  from './components/Card1'; //Function Component
 import Card2 from './components/Card2'; //Class component
 
 
+function Welcome1(){
+  return <h1>Welcome1</h1>
+}
+
+function Welcome2(){
+  return React.createElement("h1", {}, "welcome2");
+}
+
+function Todo(){
+  return <div>
+    <p>Todo Tittle</p>
+    <p>Todo desc</p>
+  </div>
+}
+
+function Todo2(){
+  return React.createElement("div", {}, 
+    React.createElement("p", {}, "Todotitle2"),
+    React.createElement("p", {}, "Tododesc2")
+  )
+}
 
 function App(){
   
   return(
     <div>
-      <Card1 name = "card1" desc = "this is card1"/>
-      <Card2 name = "card2" desc = "this is card2"/>
+      <Welcome1 />
+      <Welcome2 />
 
+      <Todo></Todo>
+      <Todo2></Todo2>
     </div>
   )
 
