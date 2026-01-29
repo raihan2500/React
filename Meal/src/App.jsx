e9b6ea042c2895b53faa182eea003cb0a403ac96
Raihan Ahmed <raiihan2500@gmail.com>
2026-01-30 05:25:26 +0600

    Initial stage

---
+++ Meal/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Meals from './components/Meals'
+
+function App() {
+
+  return (
+    <div>
+      <Meals/>
+    </div>
+  )
+}
+
+export default App
---
+++ Meal/src/components/Meals.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react'
+import '../App.css';
+
+/*
+  www.themealdb.com/api/json/v1/1/categories.php 
+  www.themealdb.com/api/json/v1/1/filter.php?c=Seafood 
+
+*/
+const categories = 'https://www.themealdb.com/api/json/v1/1/categories.php';
+const filerByCategory = 'www.themealdb.com/api/json/v1/1/filter.php?c=';
+const filterById = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=';
+
+const demo = {
+  "categories": [
+    {
+      "idCategory": "1",
+      "strCategory": "Beef",
+      "strCategoryThumb": "https://www.themealdb.com/images/category/beef.png",
+      "strCategoryDescription": "Beef is the culinary name for meat from cattle, particularly skeletal muscle. Humans have been eating beef since prehistoric times.[1] Beef is a source of high-quality protein and essential nutrients.[2]"
+    },
+    
+  ]
+}
+
+export default function Meals() {
+  const [lists, setLists] = useState(null);
+  useEffect(() =>{
+    fetch(categories)
+    .then((res) => {return res.json();})
+    .then((res) => {
+      setLists(res);
+      console.log(res);
+    })
+  },[])
+  
+  return (
+    <div>
+      <h1>Meal</h1>
+      {lists && (
+        <div>
+          {lists.categories.map(category => (
+            <div className='categories' key={category.idCategory} >
+              <img src={category.strCategoryThumb} alt="" />
+              <h3> {category.strCategory} </h3>
+              <p>{category.strCategoryDescription}</p>
+            </div>
+          ))}
+          
+        </div>
+      )}
+
+    </div>
+  )
+}
