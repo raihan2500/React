b27a30af7cd92712af79709229ede805a63fca84
Raihan Ahmed <raiihan2500@gmail.com>
2026-01-30 02:27:19 +0600

    Class 28: Update state based on previous state

--- project1/src/App.jsx
+++ project1/src/App.jsx
@@ -1,17 +1,40 @@
 import './App.css'
-import React from 'react'
+import React, { useState } from 'react'
 import State from './State'
 import Conditional_Rendering from './conditional_rendering'
 import EVENT_HANDLER_CLASS from './EVENT_HANDLER_CLASS'
 import EVENT_BINDING from './EVENT_BINDING'
 import HOOKS_USESTATE from './HOOKS_USESTATE'
 import HOOKS_USESTATE2 from './HOOKS_USESTATE/indexf'
+
+
+//Update state based on previous state
+
 function App(){
-  
+  const [count, setCount] = useState(0);
+  const increment = () =>{
+    
+    //by default erokom hoy
+    // setCount(count + 1); //0 + 1
+    // setCount(count + 1); //0 + 1
+    // setCount(count + 1); //0 + 1
+
+    setCount(count => count + 1); //0 + 1
+    setCount(count => count + 1); //1 + 1
+    setCount(count => count + 1); //2 + 1
+    
+    //We can also write like this
+    setCount((prev)=>prev + 1);
+    setCount((count)=> count + 1)
+    setCount((count) =>{
+      return count + 1;
+    })
+
+  }
   return(
     <div>
-      <HOOKS_USESTATE />
-      <HOOKS_USESTATE2 />
+      <h1>count: {count}</h1>
+      <button onClick={increment}>+</button>
     </div>
   )
 
